40b0a3311dc4863972c6efa6e3c6e79f0f344a29
thienngan3004 <thienngan9182@gmail.com>
2026-05-13 23:47:03 +0700

    dieu chinh connect toi db

--- src/main/java/com/example/schoolmanager/model/Student.java
+++ src/main/java/com/example/schoolmanager/model/Student.java
@@ -1,31 +1,31 @@
 package com.example.schoolmanager.model;
 
-
 import jakarta.persistence.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "students")
 public class Student {
     @Id
     @GeneratedValue
-    @UuidGenerator   // Hibernate 6+
-    @Column(columnDefinition = "UNIQUEIDENTIFIER",
-            updatable = false,
-            nullable = false)
+    @UuidGenerator // Hibernate 6+
+    // @Column(columnDefinition = "UNIQUEIDENTIFIER",  -- bo doan nay do conflict de data type giua sql va progressSQL
+    // updatable = false,
+    // nullable = false)
     private UUID id;
 
-    //  NVARCHAR trên SQL Server
-    @Column(columnDefinition = "NVARCHAR(100)")
+    // NVARCHAR trên SQL Server
+    // project duoc chay tren ca sql va progressSQL , go bo thuoc tinh columnDefinition de hibernate tu dong anh xa sang datatype tuong ung cho tung loai database
+    @Column(length = 100)
     private String name;
-
-    @Column(columnDefinition = "NVARCHAR(100)")
+    @Column(length = 100)
     private String email;
-        // ===== Constructor =====
-    public Student() {}
+
+    // ===== Constructor =====
+    public Student() {
+    }
 
     public Student(String name, String email) {
         this.name = name;
@@ -33,12 +33,27 @@ public class Student {
     }
 
     // ===== Getter Setter =====
-    public UUID getId() { return id; }
-    public void setId(UUID id) { this.id = id; }
+    public UUID getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
